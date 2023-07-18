f4e23055a463dbfdf29ba463c509f18922ce0d7c
Brad Blanche <88532767+Timajeff@users.noreply.github.com>
2023-07-18 12:12:04 +0200

    Add files via upload

---
+++ 4- Les fonctions/Exo1.php
@@ -0,0 +1,19 @@
+<?php
+
+$var1 = 'foo';
+$var2 = [];
+$var3 = 0;
+
+echo 'var 1: ';
+echo empty($var1). PHP_EOL;
+
+echo 'var 2: ';
+echo empty($var2). PHP_EOL;
+
+echo 'var 3: ';
+echo empty($var3). PHP_EOL;
+
+//Empty, détermine si une variable est vide.
+//Le paramètres attendu en entrée est la variable que l'on souhaite vérifier.
+//La fonction retourne un booléen.
+
---
+++ 4- Les fonctions/Exo2.php
@@ -0,0 +1,13 @@
+<?php
+
+$languages = ['PHP', 'HTML', 'JavaScript', 'C++', 'Python'];
+
+// Début du code à remplacer
+
+$listOfLanguages = implode(', ', $languages);
+// utilisation de la fonction implode pour passer d'un tableau à une liste.
+
+// Fin du code à remplacer
+
+// Affichera : PHP,HTML,JavaScript,C++,Python,
+echo $listOfLanguages;
