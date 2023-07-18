056d7ff767c6a02a80b1a8806844f76d94d004a5
Brad Blanche <88532767+Timajeff@users.noreply.github.com>
2023-07-18 12:15:22 +0200

    Add files via upload

---
+++ 18-La programmation Orientée Objet  concepts de base/Exo_Défi/Logger.php
@@ -0,0 +1,24 @@
+<?php
+
+class Logger
+{
+    public $file;
+
+    public function __construct()
+    {
+        $this->file = fopen(date('Y-m-d').'.log', 'a');
+        $this->log('Ouverture des logs');
+    }
+
+    public function log(string $message)
+    {
+        fwrite($this->file,'['.date('Y-m-d H:i:s').'] '.$message.PHP_EOL);
+    }
+
+    public function __destruct()
+    {
+        $this->log('Fermeture des Logs');
+        fclose($this->file);
+    }
+}
+
